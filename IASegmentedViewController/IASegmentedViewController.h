faaa452b5b46d073ea7fc629b3e22b41ed74d8cb
Mark Adams <mark.adams86@gmail.com>
2011-12-20 10:44:19 -0800

    Added IASegmentedViewController stub

---
+++ IASegmentedViewController/IASegmentedViewController.h
@@ -0,0 +1,13 @@
+//
+//  IASegmentedViewController.h
+//  IASegmentedViewController
+//
+//  Created by Mark Adams on 12/20/11.
+//  Copyright (c) 2011 Interstellar Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IASegmentedViewController : UIViewController
+
+@end
