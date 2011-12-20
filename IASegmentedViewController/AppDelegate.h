77f37511529c2fd4d16a2eb05dca344a7e51d565
Mark Adams <mark.adams86@gmail.com>
2011-12-20 10:33:25 -0800

    Initial commit

---
+++ IASegmentedViewController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IASegmentedViewController
+//
+//  Created by Mark Adams on 12/20/11.
+//  Copyright (c) 2011 Interstellar Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
